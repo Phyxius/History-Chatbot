a15414547c9289da377516d407e139b5c0a9f384
Shea Polansky <env.laser@gmail.com>
2016-09-15 18:53:04 -0600

    Basic NLP test

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args)
     {
+        Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
+        List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
+        String firstPOSTag = sent.posTag(0);   // NNP
+        System.out.println(String.join(" ", sent.posTags()));
     }
 }
