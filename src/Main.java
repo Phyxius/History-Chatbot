040690c3968ecc2142d7cbff46575581aa67ce13
Shea Polansky <env.laser@gmail.com>
2016-09-15 18:43:45 -0600

    Added Stanford NLP references

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 /**
  * Created by Shea on 2016-09-15.
  */
+import edu.stanford.nlp.simple.*;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
     }
 }
