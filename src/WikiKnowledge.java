1feaafb0414a3cfda90b325393f919a4f8121ef2
Shea <env.laser@gmail.com>
2016-09-17 21:57:47 -0600

    Added getBattleResult Reformatted some stuff Added stripFormatting method and used it

--- src/WikiKnowledge.java
+++ src/WikiKnowledge.java
@@ -12,7 +12,9 @@ import java.util.regex.Pattern;
 public class WikiKnowledge {
     private static final Wiki wiki;
     private static final String INFO_BOX_FLAG_TEXT = "{{Infobox military conflict";
-    private static final Pattern REF_STRIP_PATTERN = Pattern.compile("<ref>.+</ref>"); //strips out ref tags that may throw off matching
+    private static final Pattern REF_STRIP_PATTERN = Pattern.compile("<ref>.+</ref>|<ref name=.+/>"); //strips out ref tags that may throw off matching
+    private static final Pattern LINK_STRIP_PATTERN = Pattern.compile("\\[{2}(.+?\\|)?(?<text>.+?)\\]{2}");
+    private static final Pattern COMMENT_STRIP_PATTERN = Pattern.compile("<!--.+-->");
     private static final Pattern BATTLE_KILLED_PATTERN =
             Pattern.compile("((killed|dead): (?<count1>[0-9,]+))|((?<count2>[0-9,]+) (\\w+)?(dead|killed))");
     private static final Pattern BATTLE_LOCATION_PATTERN =
@@ -20,6 +22,7 @@ public class WikiKnowledge {
     private static final Pattern BATTLE_DATE_PATTERN =
             Pattern.compile("\\|\\s*date\\s*=\\s*.*?(?<month>January|February|March|April|May|June|July|August|September|October|November|December).+?(?<year>\\d{3,4}( ?BC(E?))?)");
     private static final String[] killedPatternCountGroups = {"count1", "count2"};
+    private static final Pattern BATTLE_RESULT_PATTERN = Pattern.compile("\\|\\s*result\\s*=\\s*(?<result>.+?)([,.]|\n)");
 
     static {
         wiki = new Wiki("en.wikipedia.org");
@@ -29,9 +32,8 @@ public class WikiKnowledge {
     public static Optional<String> getBattleCasualties(String battleName, int chanceOfNotKnowing) {
         if (chanceOfNotKnowing > 0 && !KnowledgeChance.doesKnowAbout(battleName, chanceOfNotKnowing))
             return Optional.empty();
-        Optional<String> infoBoxText = extractBattleInfoBox(battleName);
+        Optional<String> infoBoxText = extractBattleInfoBox(battleName).map(WikiKnowledge::stripMarkup);
         if (!infoBoxText.isPresent()) return Optional.empty();
-        infoBoxText = Optional.of(REF_STRIP_PATTERN.matcher(infoBoxText.get()).replaceAll(""));
         final Matcher matcher = BATTLE_KILLED_PATTERN.matcher(infoBoxText.get());
         int totalKilled = 0;
         while (matcher.find()) {
@@ -48,9 +50,8 @@ public class WikiKnowledge {
     public static Optional<String> getBattleLocation(String battleName, int chanceOfNotKnowing) {
         if (chanceOfNotKnowing > 0 && !KnowledgeChance.doesKnowAbout(battleName, chanceOfNotKnowing))
             return Optional.empty();
-        Optional<String> infoBoxText = extractBattleInfoBox(battleName);
+        Optional<String> infoBoxText = extractBattleInfoBox(battleName).map(WikiKnowledge::stripMarkup);
         if (!infoBoxText.isPresent()) return Optional.empty();
-        infoBoxText = Optional.of(REF_STRIP_PATTERN.matcher(infoBoxText.get()).replaceAll(""));
         Matcher matcher = BATTLE_LOCATION_PATTERN.matcher(infoBoxText.get().replaceAll("[\\[\\]]", ""));
         if (!matcher.find()) return Optional.empty();
         String location = matcher.group("location");
@@ -61,15 +62,27 @@ public class WikiKnowledge {
     public static Optional<String> getBattleDate(String battleName, int chanceOfNotKnowing) {
         if (chanceOfNotKnowing > 0 && !KnowledgeChance.doesKnowAbout(battleName, chanceOfNotKnowing))
             return Optional.empty();
-        Optional<String> infoBoxText = extractBattleInfoBox(battleName);
+        Optional<String> infoBoxText = extractBattleInfoBox(battleName).map(WikiKnowledge::stripMarkup);
         if (!infoBoxText.isPresent()) return Optional.empty();
-        infoBoxText = Optional.of(REF_STRIP_PATTERN.matcher(infoBoxText.get()).replaceAll(""));
         Matcher matcher = BATTLE_DATE_PATTERN.matcher(infoBoxText.get());
         if (!matcher.find()) return Optional.empty();
         if (matcher.group("year") == null || matcher.group("year").equals(""))
             return Optional.empty();
         return Optional.of((matcher.group("month") != null ?
                 matcher.group("month") + " " : "") + matcher.group("year"));
+    }
+
+    public static Optional<String> getBattleResult(String battleName, int chanceOfNotKnowing)
+    {
+        if (chanceOfNotKnowing > 0 && !KnowledgeChance.doesKnowAbout(battleName, chanceOfNotKnowing))
+            return Optional.empty();
+        Optional<String> infoBoxText = extractBattleInfoBox(battleName).map(WikiKnowledge::stripMarkup);
+        if (!infoBoxText.isPresent()) return Optional.empty();
+        //strip refs and links
+        Matcher matcher = BATTLE_RESULT_PATTERN.matcher(infoBoxText.get());
+        if (!matcher.find())
+            return Optional.empty();
+        return Optional.of(matcher.group("result"));
     }
 
     /**
@@ -112,5 +125,13 @@ public class WikiKnowledge {
     public static String roundNumberString(String numberString) {
         return numberString.substring(0, 1) + new String( //one liner way of generating a string with n zeroes, because Java
                 new char[numberString.length() - 1]).replaceAll("\0", "0");
+    }
+
+    private static String stripMarkup(String text)
+    {
+        text = REF_STRIP_PATTERN.matcher(text).replaceAll("");
+        text = LINK_STRIP_PATTERN.matcher(text).replaceAll("${text}");
+        text = COMMENT_STRIP_PATTERN.matcher(text).replaceAll("");
+        return text;
     }
 }
